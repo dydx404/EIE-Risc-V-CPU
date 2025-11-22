09bf0193cef401150ccea9e0797ede8f813ab389
Seth Gobina <seth.gobina@gmail.com>
2025-11-22 22:26:53 +0000

    Control and Extend module, testing and verification pending

--- rtl/single_cycle/immgen/Extend.sv
+++ rtl/single_cycle/immgen/Extend.sv
@@ -6,10 +6,11 @@ module Extend(
 
     always @* begin
         case(immsrc)
-            2'b00: immext = {{20{imm[31]}}, imm[31:20]};
-            2'b10: immext = {{19{imm[31]}}, imm[31], imm[7], imm[30:25], imm[11:8], 1'b0};
-            2'b01: immext = {{20{imm[31]}},imm[31:25], imm[11:7]};
-            2'b11: immext = {{11{imm[31]}}, imm[31], imm[19:12], imm[20], imm[30:21], 1'b0};
+            2'b00: immext = {{20{imm[31]}}, imm[31:20]}; //I-type
+            2'b01: immext = {{20{imm[31]}},imm[31:25], imm[11:7]}; //S-Type
+            2'b10: immext = {{19{imm[31]}}, imm[31], imm[7], imm[30:25], imm[11:8], 1'b0}; //B-Type
+            2'b11: immext = {{11{imm[31]}}, imm[31], imm[19:12], imm[20], imm[30:21], 1'b0}; //J-Type
+            default: immext = {{32{1'b0}}};
         endcase
     end
 
