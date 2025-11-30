ca9afbb7e8433565b167c536f3035fb12a70b283
dydx404 <yd1723@ic.ac.uk>
2025-11-30 15:35:33 +0000

    Add MEM_STAGE, DataMemory copy, WB_STAGE

---
+++ rtl/pipelined/wb-stage/WB_STAGE.sv
@@ -0,0 +1,21 @@
+module WB_STAGE (
+    // From MEM/WB pipeline register
+    input  logic [31:0] aluResultW,
+    input  logic [31:0] readDataW,
+    input  logic [31:0] pcPlus4W,
+    input  logic [1:0]  resultSrcW,
+
+    // Output to register file
+    output logic [31:0] resultW
+);
+
+    always_comb begin
+        unique case (resultSrcW)
+            2'b00: resultW = aluResultW;   // ALU instruction
+            2'b01: resultW = readDataW;    // Load instruction
+            2'b10: resultW = pcPlus4W;     // JAL / JALR
+            default: resultW = aluResultW;
+        endcase
+    end
+
+endmodule
